731481b5335aa085732f7a1a9bd14f23038f2e42
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2019-05-24 16:59:18 -0400

    Added illuminationFromObjectiveWithOptotune

--- Code illuminateur.py
+++ Code illuminateur.py
@@ -79,9 +79,50 @@ class Sparq:
 
         return illumination
 
+    def illuminationFromObjectiveWithOptotune():
+    # Add tunable lens EL-16-40 and EL-10-30
+        optotuneFocal = 100
+        L1 = Lens(f=40, diameter=30, label="$L_1$")
+        L2 = Lens(f=30, diameter=20, label="$L_2$")
+        L3 = Lens(f=-35, diameter=22, label="$L_3$")
+        L4 = Lens(f=75, diameter=32, label="$L_4$")
+        LExc = Lens(f=45, diameter=35, label="Exc")
+        Optotune = Lens(f=optotuneFocal, diameter=10, label='Optotune')
+        obj = olympus.XLUMPlanFLN20X()
+        obj.flipOrientation()
+
+        illumination = ImagingPath()
+        illumination.label = "Sparq illumination"
+        illumination.objectHeight = 0.5 + 0.2  # mm maximum, include diffuse spot size
+        illumination.fanAngle = 1.05  # NA = 1.05
+        illumination.fanNumber = 11
+        illumination.rayNumber = 3
+        illumination.showImages = False
+
+        illumination.append(obj)
+        illumination.append(Space(d=40))
+        illumination.append(Optotune)
+        illumination.append(Space(d=45+47.5))
+        illumination.append(L1)
+        illumination.append(Space(d=40))
+        illumination.append(Aperture(diameter=30, label="AF"))
+        illumination.append(Space(d=20))
+        illumination.append(Aperture(diameter=30, label="CF"))
+        illumination.append(Space(d=30))
+        illumination.append(L2)
+        illumination.append(Space(d=57))
+        illumination.append(L3)
+        illumination.append(Space(d=40))
+        illumination.append(L4)
+        illumination.append(Space(d=20))
+        illumination.append(LExc)
+        illumination.append(Space(d=45))
+
+        return illumination
 
 
 if __name__ == "__main__":
 
     Sparq.illuminationFromObjective().display()
     Sparq.illuminationFromSource().display()
+    Sparq.illuminationFromObjectiveWithOptotune().display()
