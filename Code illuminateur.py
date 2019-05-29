e74c545a25711a0fccf054c43ac45980a4b236a5
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2019-05-29 11:40:40 -0400

    Added illumination with optotune AND divergent lens

--- Code illuminateur.py
+++ Code illuminateur.py
@@ -152,9 +152,52 @@ class Sparq:
         illumination.append(Aperture(diameter=30, label="AF"))
         illumination.append(Space(d=40))
         illumination.append(L1)
-        illumination.append(Space(d=45 + 47.5))
+        illumination.append(Space(d=75))
         illumination.append(Optotune)
+        illumination.append(Space(d=45))
+        illumination.append(obj)
+
+        return illumination
+
+    def illuminationFromSourceWithOptotuneAndDivergentLens():
+    # Add tunable lens EL-16-40 and EL-10-30
+        optotuneFocal = 58.5
+        L1 = Lens(f=40, diameter=30, label="$L_1$")
+        L2 = Lens(f=30, diameter=20, label="$L_2$")
+        L3 = Lens(f=-35, diameter=22, label="$L_3$")
+        L4 = Lens(f=75, diameter=32, label="$L_4$")
+        LExc = Lens(f=45, diameter=35, label="Exc")
+        Optotune = Lens(f=optotuneFocal, diameter=16, label='Optotune')
+        L5 = Lens(f=-75, diameter=35, label='Divergent lens')
+        obj = olympus.XLUMPlanFLN20X()
+
+        illumination = ImagingPath()
+        illumination.label = "Sparq illumination with Optotune and divergent lens"
+        illumination.objectHeight = 3.15
+        illumination.fanAngle = 0.5  # NAdiffuser=1 alors mettre 0.5 ou 1?
+        illumination.fanNumber = 11
+        illumination.rayNumber = 3
+        illumination.showImages = False
+
+        illumination.append(Space(d=45))
+        illumination.append(LExc)
+        illumination.append(Space(d=20))
+        illumination.append(L4)
         illumination.append(Space(d=40))
+        illumination.append(L3)
+        illumination.append(Space(d=57))
+        illumination.append(L2)
+        illumination.append(Space(d=30))
+        illumination.append(Aperture(diameter=30, label="CF"))
+        illumination.append(Space(d=20))
+        illumination.append(Aperture(diameter=30, label="AF"))
+        illumination.append(Space(d=40))
+        illumination.append(L1)
+        illumination.append(Space(d=65))
+        illumination.append(L5)
+        illumination.append(Space(d=10))
+        illumination.append(Optotune)
+        illumination.append(Space(d=45))
         illumination.append(obj)
 
         return illumination
@@ -222,5 +265,6 @@ if __name__ == "__main__":
     Sparq.illuminationFromSource().display()
     Sparq.illuminationFromObjectiveWithOptotune().display()
     Sparq.illuminationFromSourceWithOptotune().display()
+    Sparq.illuminationFromSourceWithOptotuneAndDivergentLens().display()
     Sparq.illuminationFromObjectiveToCamera().display()
     Sparq.tracingForIlluminatorMagnification().display()
