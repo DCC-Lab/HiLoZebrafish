5ddde14d66e1425425d23164051d94b23e37cc04
Valérie <valerie.pineau-noel.1@ulaval.ca>
2019-06-11 18:16:40 -0400

    Update signal_processing_functions.py

--- Code Optotune lens à Guillaume Allain/First Release/signal_processing_functions.py
+++ Code Optotune lens à Guillaume Allain/First Release/signal_processing_functions.py
@@ -1,4 +1,5 @@
-import numpy as np 
+# coding=utf-8
+import numpy as np
 from scipy import signal
 
 def normalize_set(set1,set2):
@@ -13,11 +14,11 @@ def normalize_set(set1,set2):
 
 def peak_detect_periodic(set,fq,scanrate,sensibility=0.1):
 	'''Returns all the peak detection for a periodic signal of a given frequency. May not work.'''
-	spectrum = np.fft.fft(set)
-	freqseq = np.fft.fftfreq(len(spectrum),1/float(scanrate))
+	spectrum = np.fft.fft(set)										# Faire deux fois la Transformée de Fourier discrète?
+	freqseq = np.fft.fftfreq(len(spectrum),1/float(scanrate))		# retourne un tableau de fréquences des éléments de sortie correspondants
 	spectrum = spectrum * np.exp(-np.power(freqseq - (fq+2*fq)/2 , 2.) / (2 * np.power(300, 2.)))
 	spectrum[np.where(np.logical_or(np.less(abs(freqseq),fq*0.2),np.greater(abs(freqseq),fq*13)))] = 0
-	setmod = np.real(np.fft.ifft(spectrum))
+	setmod = np.real(np.fft.ifft(spectrum))		# Retourne la partie réelle de l'argument. Si l'argument est réel, retourne le type de l'argument. Sinon, (si l'argument est complexe), le type de l'argument est float.
 	peakind = np.array(signal.argrelmax(np.array([x if x>sensibility*setmod.max() else 0 for x in setmod])))
 	return peakind
 
