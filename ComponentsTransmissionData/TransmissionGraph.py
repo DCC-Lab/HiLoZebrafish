c0cbb5e9fae7349e2180911029aecfe5f8cd1b14
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2019-07-02 10:07:24 -0400

    Added GFP filter 500-590

--- ComponentsTransmissionData/TransmissionGraph.py
+++ ComponentsTransmissionData/TransmissionGraph.py
@@ -2,24 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 filename1 = "Filter_BA510-550\\Transmission_1_510550.txt"
-filename2 = "Filter_BA510-550\\Transmission_2_510550.txt"
-filename3 = "Filter_BA510-550\\Transmission_3_510550.txt"
-filename4 = "Filter_BA510-550\\Transmission_4_510550.txt"
+filename2 = "Filter_500-590\\Transmission_5_500590.txt"
+# filename3 = "Filter_500-590\\Transmission_6_500590.txt"
+# filename4 = "Filter_500-590\\Transmission_7_500590.txt"
 
 x1 = np.loadtxt(filename1, usecols=0)
 y1 = np.loadtxt(filename1, usecols=1)
 
-# x2 = np.loadtxt(filename2, usecols=0)
-# y2 = np.loadtxt(filename2, usecols=1)
-#
+x2 = np.loadtxt(filename2, usecols=0)
+y2 = np.loadtxt(filename2, usecols=1)
+
 # x3 = np.loadtxt(filename2, usecols=0)
 # y3 = np.loadtxt(filename2, usecols=1)
 #
 # x4 = np.loadtxt(filename2, usecols=0)
 # y4 = np.loadtxt(filename2, usecols=1)
 
-Data0, = plt.plot(x1, y1, color='k', label='first')
-# Data1, = plt.plot(x2, y2, color='b', label='second')
+Data0, = plt.plot(x1, y1, color='k', label='Filter 510-550')
+Data1, = plt.plot(x2, y2, color='b', label='Filter 500-590')
 # Data2, = plt.plot(x3, y3, color='g', label='third')
 # Data3, = plt.plot(x4, y4, color='y', label='fourth')
 plt.xlim([450, 650])
@@ -29,7 +29,7 @@ plt.xticks(fontsize=16)
 plt.xlabel("Wavelength [nm]", fontsize=18)
 plt.ylabel("Transmission [%]", fontsize=18)
 # plt.grid(True)
-# plt.legend(handles=[Data0, Data1, Data2, Data3], fontsize=18)
+plt.legend(handles=[Data0, Data1], fontsize=18)
 plt.minorticks_on()
 fig = plt.gcf()
 fig.set_size_inches(12, 7)
