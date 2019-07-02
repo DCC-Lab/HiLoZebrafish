5da36f05bd2b24e1007559bd51ce404b1fc8aa98
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2019-07-02 10:23:48 -0400

    Clean code

--- ComponentsTransmissionData/TransmissionGraph.py
+++ ComponentsTransmissionData/TransmissionGraph.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 
 filename1 = "Filter_BA510-550\\Transmission_1_510550.txt"
 filename2 = "Filter_500-590\\Transmission_5_500590.txt"
-# filename3 = "Filter_500-590\\Transmission_6_500590.txt"
-# filename4 = "Filter_500-590\\Transmission_7_500590.txt"
 
 x1 = np.loadtxt(filename1, usecols=0)
 y1 = np.loadtxt(filename1, usecols=1)
@@ -12,23 +10,15 @@ y1 = np.loadtxt(filename1, usecols=1)
 x2 = np.loadtxt(filename2, usecols=0)
 y2 = np.loadtxt(filename2, usecols=1)
 
-# x3 = np.loadtxt(filename2, usecols=0)
-# y3 = np.loadtxt(filename2, usecols=1)
-#
-# x4 = np.loadtxt(filename2, usecols=0)
-# y4 = np.loadtxt(filename2, usecols=1)
-
 Data0, = plt.plot(x1, y1, color='k', label='Filter 510-550')
 Data1, = plt.plot(x2, y2, color='b', label='Filter 500-590')
-# Data2, = plt.plot(x3, y3, color='g', label='third')
-# Data3, = plt.plot(x4, y4, color='y', label='fourth')
 plt.xlim([450, 650])
 plt.ylim([0, 100])
 plt.yticks(fontsize=16)
 plt.xticks(fontsize=16)
 plt.xlabel("Wavelength [nm]", fontsize=18)
 plt.ylabel("Transmission [%]", fontsize=18)
-# plt.grid(True)
+plt.grid(False)
 plt.legend(handles=[Data0, Data1], fontsize=18)
 plt.minorticks_on()
 fig = plt.gcf()
