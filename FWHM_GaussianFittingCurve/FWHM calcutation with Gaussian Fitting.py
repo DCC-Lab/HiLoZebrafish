f9b44750a88e7c48be45bbb1f077688a3e84de98
Valérie <valerie.pineau-noel.1@ulaval.ca>
2019-07-11 11:20:02 -0400

    Calculations of FWHM

--- FWHM_GaussianFittingCurve/FWHM calcutation with Gaussian Fitting.py
+++ FWHM_GaussianFittingCurve/FWHM calcutation with Gaussian Fitting.py
@@ -1,9 +1,11 @@
 from math import sqrt, log, exp
 from sympy import symbols, solve, Function, Eq, solveset
 
-a =	68106.8969
-b =	3.56844
-c = 0.47129
+a =	36348.8916
+b = 2.36981
+c = 0.42731
+print(a, b, c, sep='\n')
+
 y = a/2
 print(y)
 x = symbols('x', real=True)
@@ -12,8 +14,10 @@ f = (-(1 / (2*c**2)) * x**2) + ((b/c**2) * x) + (-(b**2 / (2*c**2)) - log(y/a))
 print(f)
 
 solutions = solve(f, x)
-print("..", solutions)
+print(solutions)
 
+FWHM = solutions[1] - solutions[0]
+print(FWHM)
 
 
 
