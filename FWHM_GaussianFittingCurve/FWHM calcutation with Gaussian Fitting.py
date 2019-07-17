93d6d65a089b02799aa53afaccd61a7df145cab0
Valérie <valerie.pineau-noel.1@ulaval.ca>
2019-07-17 13:20:35 -0400

    Calculations FWHM

--- FWHM_GaussianFittingCurve/FWHM calcutation with Gaussian Fitting.py
+++ FWHM_GaussianFittingCurve/FWHM calcutation with Gaussian Fitting.py
@@ -25,3 +25,20 @@ print(FWHM)
 # fwhm = 2 * stddev * sqrt(2 * log(10))
 # print(fwhm)
 
+'''Gaussian fit curve'''
+
+a =	93.18842
+b =	107.92623
+c = 265.97402
+d = 65.37462
+y = (b-a)/2 + a
+x = symbols('x', real=True)
+
+f = ((-1 / (2 * d**2)) * x**2) + (((c / d**2) ) * x) - (c**2 / (2 * d**2)) - log((y-a) / (b-a))
+
+solutions = solve(f, x)
+
+FWHM = solutions[1] - solutions[0]
+
+
+print(a, b, c, d, solutions[0], solutions[1], FWHM, sep=' ')
