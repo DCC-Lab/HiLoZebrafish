70a17f9ef6d9e828332c1ced33213f4b58f39eb5
Valérie <valerie.pineau-noel.1@ulaval.ca>
2019-07-22 08:20:40 -0400

    Calculations on FWHM_GaussianFittingCurve.py

--- FWHM_GaussianFittingCurve/FWHM calcutation with Gaussian Fitting.py
+++ FWHM_GaussianFittingCurve/FWHM calcutation with Gaussian Fitting.py
@@ -1,23 +1,23 @@
 from math import sqrt, log, exp
 from sympy import symbols, solve, Function, Eq, solveset
 
-a =	66377.8508
-b = 3.93834
-c = 0.71691
-print(a, b, c, sep='\n')
-
-y = a/2
-print(y)
-x = symbols('x', real=True)
-
-f = (-(1 / (2*c**2)) * x**2) + ((b/c**2) * x) + (-(b**2 / (2*c**2)) - log(y/a))
-print(f)
-
-solutions = solve(f, x)
-print(solutions)
-
-FWHM = solutions[1] - solutions[0]
-print(FWHM)
+# a =	66377.8508
+# b = 3.93834
+# c = 0.71691
+# print(a, b, c, sep='\n')
+#
+# y = a/2
+# print(y)
+# x = symbols('x', real=True)
+#
+# f = (-(1 / (2*c**2)) * x**2) + ((b/c**2) * x) + (-(b**2 / (2*c**2)) - log(y/a))
+# print(f)
+#
+# solutions = solve(f, x)
+# print(solutions)
+#
+# FWHM = solutions[1] - solutions[0]
+# print(FWHM)
 
 
 
@@ -27,16 +27,18 @@ print(FWHM)
 
 '''Gaussian fit curve'''
 
-a =	93.18842
-b =	107.92623
-c = 265.97402
-d = 65.37462
+a =	91.2742
+b =	95.32937
+c = 1023.99992
+d = 128.54601
 y = (b-a)/2 + a
 x = symbols('x', real=True)
 
 f = ((-1 / (2 * d**2)) * x**2) + (((c / d**2) ) * x) - (c**2 / (2 * d**2)) - log((y-a) / (b-a))
 
+
 solutions = solve(f, x)
+
 
 FWHM = solutions[1] - solutions[0]
 
