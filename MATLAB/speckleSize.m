1b39288eac2d3d94fe298540309855dbc162f2c8
Daniel Côté <dccote@novajo.ca>
2019-12-21 21:19:20 -0500

    Zero padding on FFT to improve concordance with normcorr2

--- MATLAB/speckleSize.m
+++ MATLAB/speckleSize.m
@@ -1,18 +1,14 @@
-files = {'speckle0.tiff','speckle1pouce_12ms.tiff','speckle2pouces_30ms.tiff','speckle3pouces_50ms.tiff','speckle4pouces_80ms.tiff','speckle5pouces_120ms.tiff','speckle6pouces_150ms.tiff'};
-distFiles = [0,1,2,3,4,5,6];
-
-s = imread(files{i});
-
+files = {'speckle1pouce_12ms.jpg','speckle2pouces_30ms.jpg'};
+distFiles = [1,2];
 
 widths = [];
 distances = [];
-for i = 2:7
-    files{i}
+for i = size(files,1)+1
     s = imread(files{i});
     theSize = 200;
     s = cropCenter(s,theSize);
-    [x,y]=speckleFWHM(s)
-    widths = [widths, x];
+    [x,y,c]=speckleFWHM(s);
+    widths = [widths, x]
     distances = [distances, distFiles(i)];
 end
 
