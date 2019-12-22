f8d67db6c8bdfaa38261312af0eb7f6104eacb98
Daniel Côté <dccote@novajo.ca>
2019-12-21 21:11:50 -0500

    Improvement with MATLAB findpeaks

--- MATLAB/fwhm.m
+++ MATLAB/fwhm.m
@@ -2,16 +2,17 @@ function [ wx, wy] = fwhm(normCorr)
 
 [yPeak, xPeak] = peakCoordinates(normCorr);
 maxValue = normCorr(yPeak, xPeak);
-
-
 delta= 0.01;
-a = normCorr(:,yPeak);
-range = [1:delta:size(a)]
+
+a = normCorr(yPeak,:)';
+range = [1:delta:size(a)];
 morePoints = interp1([1:size(a)],a,range);
 wy = size(find(morePoints>=0.5*maxValue),2)*delta;
+[p,loc,wy] = findpeaks(morePoints,range,'SortStr','descend','MinPeakWidth',2,'NPeaks',1);
 
 a = normCorr(:,xPeak);
-range = [1:delta:size(a)]
+range = [1:delta:size(a)];
 morePoints = interp1([1:size(a)],a,range);
 wx = size(find(morePoints>=0.5*maxValue),2)*delta;
+[p,loc,wx] = findpeaks(morePoints,range,'SortStr','descend','MinPeakWidth',2,'NPeaks',1);
 
--- MATLAB/speckleFWHMFourier.m
+++ MATLAB/speckleFWHMFourier.m
@@ -1,7 +1,9 @@
 function [wx, wy, normCorr] = speckleFWHMFourier(image)
+    w1 = size(image,1);
+    w2 = size(image,2);
     image = double(image) - mean(mean(double(image)));
-    image = image .* (hann(size(image,1))*hann(size(image,2))')
-    powerSpectrum = fft2(image).*conj(fft2(image));
+    image = image .* (hann(size(image,1))*hann(size(image,2))');
+    powerSpectrum = fft2(image,2*w1,2*w2).*conj(fft2(image,2*w1,2*w2));
     corr = ifft2(powerSpectrum);
     normCorr = corr./max(max(corr));
     normCorr = fftshift(normCorr,1);
