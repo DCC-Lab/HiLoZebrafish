1ff548fc45f48859a243d13cafee5b1e81a451e4
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-03-26 14:09:59 -0400

    Tests on Speckles images - Mean speckle size calculaitons

--- MATLAB/speckleSize.m
+++ MATLAB/speckleSize.m
@@ -1,76 +1,92 @@
-files = {'20190924-200ms_20mW_Ave15_Gray_20X0.75_1.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_2.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_3.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_4.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_5.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_6.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_7.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_8.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_9.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_10.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_11.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_12.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_13.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_14.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_15.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_16.tif','20190924-200ms_20mW_Ave15_Gray_20X0.75_17.tif'};
-distFiles = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+files = {'20190924-200ms_20mW_Ave15_Gray_10X0.4_1.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_2.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_3.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_4.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_5.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_6.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_7.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_8.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_9.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_10.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_11.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_12.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_13.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_14.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_15.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_16.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_17.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_18.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_19.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_20.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_21.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_22.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_23.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_24.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_25.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_26.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_27.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_28.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_29.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_30.tif','20190924-200ms_20mW_Ave15_Gray_10X0.4_31.tif'};
+distFiles = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
 
 widths1 = [];
 distances = [];
-for i = 1:17
+for i = 1:31
     s = imread(files{i});
-    theSize = 1000;
+    theSize = 400;
     s = cropCenter(s,theSize);
-    [x,y,c]=speckleRealFWHMFourier(s);
+    [x,y,c]=speckleRealFWHM(s);
     widths1 = [widths1; x];
     distances = [distances, distFiles(i)];
 end
 
 widths1
+ 
+figure(1);
+plot(distances, widths1,'o');
 
-% figure(1);
-% plot(distances, widths1,'o');
-% 
+
+  
 % function showSpeckleSize(image, width)
-%     corr=normxcorr2(image,image);
-%     surf(corr)
+%      corr=normxcorr2(image,image);
+%      surf(corr)
 % end
 
 widths2 = [];
 distances = [];
-for i = 1:17
+for i = 1:31
     s = imread(files{i});
-    theSize = 500;
+    theSize = 250;
     s = cropCenter(s,theSize);
-    [x,y,c]=speckleRealFWHMFourier(s);
+    [x,y,c]=speckleRealFWHM(s);
     widths2 = [widths2; x];
     distances = [distances, distFiles(i)];
 end
 
 widths2
-
-% figure(2);
-% plot(distances, widths2,'o');
-
-% function showSpeckleSize(image, width)
-%     corr=normxcorr2(image,image);
-%     surf(corr)
-% end
+% 
+%  figure(2);
+%  plot(distances, widths2,'o');
+% 
+%  function showSpeckleSize(image, width)
+%      corr=normxcorr2(image,image);
+%      surf(corr)
+%  end
 
 widths3 = [];
 distances = [];
-for i = 1:17
+for i = 1:31
     s = imread(files{i});
     theSize = 100;
     s = cropCenter(s,theSize);
-    [x,y,c]=speckleRealFWHMFourier(s);
+    [x,y,c]=speckleRealFWHM(s);
     widths3 = [widths3; x];
     distances = [distances, distFiles(i)];
 end
 
 widths3
-
-% figure(3);
-% plot(distances, widths3,'o');
-
-% function showSpeckleSize(image, width)
+% 
+%  figure(3);
+%  plot(distances, widths3,'o');
+% 
+%  function showSpeckleSize(image, width)
 %     corr=normxcorr2(image,image);
 %     surf(corr)
-% end
+%  end
+
+
+widths4 = [];
+distances = [];
+for i = 1:31
+    s = imread(files{i});
+    theSize = 75;
+    s = cropCenter(s,theSize);
+    [x,y,c]=speckleRealFWHM(s);
+    widths4 = [widths4; x];
+    distances = [distances, distFiles(i)];
+end
+
+widths4
 
 widths11 = [];
 distances = [];
-for i = 1:17
+for i = 1:31
     s = medfilt2(imread(files{i}),[2,2]);
-    theSize = 1000;
+    theSize = 400;
     s = cropCenter(s,theSize);
-    [x,y,c]=speckleRealFWHMFourier(s);
+    [x,y,c]=speckleRealFWHM(s);
     widths11 = [widths11; x];
     distances = [distances, distFiles(i)];
 end
@@ -87,9 +103,9 @@ widths11
 
 widths22 = [];
 distances = [];
-for i = 1:17
+for i = 1:31
     s = medfilt2(imread(files{i}),[2,2]);
-    theSize = 500;
+    theSize = 250;
     s = cropCenter(s,theSize);
     [x,y,c]=speckleRealFWHMFourier(s);
     widths22 = [widths22; x];
@@ -109,7 +125,7 @@ widths22
 
 widths33 = [];
 distances = [];
-for i = 1:17
+for i = 1:31
     s = medfilt2(imread(files{i}),[2,2]);
     theSize = 100;
     s = cropCenter(s,theSize);
